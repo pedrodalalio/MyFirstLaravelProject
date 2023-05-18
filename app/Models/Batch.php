d02b53f9d76eddf42eeadfe83881753b66bb546c
pedrodalalio <pedrodalalio13@gmail.com>
2023-05-18 17:55:12 -0300

    First commit for challenge 2

---
+++ app/Models/Batch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Batch extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'dt_validity',
+        'qt_product',
+        'active',
+    ];
+}
